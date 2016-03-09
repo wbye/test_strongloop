6113eaab236cb124d6a5d87f980fdc4cac411e86
ywbshiwo <785534746@qq.com>
2016-03-09 21:24:10 +0800

    add slide

--- src/js/activity/201603/choose/view.js
+++ src/js/activity/201603/choose/view.js
@@ -1,57 +1,79 @@
 define([
     'swiper',
     'backbone',
-    'tpl!./template.html'
-], function (Swiper,Backbone,template) {
+    'tpl!./template.html',
+    'tpl!./template_slide.html',
+    'underscore'
+], function (Swiper, Backbone, template,template_slide,_) {
     return Backbone.View.extend({
-        template:template,
-        tagName:"div",
-        className:"activity-choose-item",
-        initialize:function(){
+        template: template,
+        tagName: "div",
+        className: "activity-choose-item",
+        initialize: function () {
             var self = this;
 
             this.on({
-                "loading":function(){
+                "loading": function () {
                     self.setViewState('loading');
                 },
-                "error":function(){
+                "error": function () {
                     self.setViewState('error');
                 },
-                "emptyview":function(){
+                "emptyview": function () {
                     self.setViewState('emptyview');
                 },
-                'complete':function(){
+                'complete': function () {
                     self.setViewState('complete');
                 },
-                "success":function(){
+                "success": function () {
                     self.setViewState('success');
                 }
             });
 
         },
-        setViewState:function(state){
-            switch (state){
-                case "loading":break;
-                case "error":break;
-                case "emptyview":break;
-                case "complete":break;
-                case "success":break;
-                default:break;
+        setViewState: function (state) {
+            switch (state) {
+                case "loading":
+                    break;
+                case "error":
+                    break;
+                case "emptyview":
+                    break;
+                case "complete":
+                    break;
+                case "success":
+                    break;
+                default:
+                    break;
             }
             return this;
         },
         render: function () {
             this.$el.html(this.template());
             this.initSwiper();
+
             return this;
         },
+        prepareImage: function () {
+            var pics = [
+                "/image/coleflower1.jpg",
+                "/image/coleflower2.jpg",
+                "/image/coleflower3.jpg",
+            ];
+
+            this.$(".swiper-wrapper").html(template_slide({items:pics}));
+        },
         initSwiper: function () {
-            var mySwiper = new Swiper(this.$('.swiper-container'), {
-                speed: 400,
-                pagination:this.$(".swiper-pagination"),
-                slidesPerView: 'auto',
-                paginationClickable: true
-            });
+            var mySwiper;
+
+            this.prepareImage();
+            setTimeout(function () {
+                mySwiper = new Swiper(this.$('.swiper-container'), {
+                    speed: 400,
+                    pagination: this.$(".swiper-pagination"),
+                    paginationClickable: true
+                });
+            }.bind(this),0)
         }
     });
 })
--- src/js/activity/activity201603.js
+++ src/js/activity/activity201603.js
@@ -5,4 +5,8 @@ require([
     'activity/201603/choose/view'
 ], function (_, $, semanticUI,ChooseView) {
     (new ChooseView().render().$el).appendTo($("#choose-list"));
+    (new ChooseView({
+        className:"activity-choose-item pic-pull-right"
+    }).render().$el).appendTo($("#choose-list"));
+
 });
