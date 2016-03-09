5015d68ad87bf4f66b38812917e80e87104816ed
ywbshiwo <785534746@qq.com>
2016-03-09 20:26:50 +0800

    add something interesting

--- src/js/activity/201603/choose/view.js
+++ src/js/activity/201603/choose/view.js
@@ -7,6 +7,39 @@ define([
         template:template,
         tagName:"div",
         className:"activity-choose-item",
+        initialize:function(){
+            var self = this;
+
+            this.on({
+                "loading":function(){
+                    self.setViewState('loading');
+                },
+                "error":function(){
+                    self.setViewState('error');
+                },
+                "emptyview":function(){
+                    self.setViewState('emptyview');
+                },
+                'complete':function(){
+                    self.setViewState('complete');
+                },
+                "success":function(){
+                    self.setViewState('success');
+                }
+            });
+
+        },
+        setViewState:function(state){
+            switch (state){
+                case "loading":break;
+                case "error":break;
+                case "emptyview":break;
+                case "complete":break;
+                case "success":break;
+                default:break;
+            }
+            return this;
+        },
         render: function () {
             this.$el.html(this.template());
             this.initSwiper();
