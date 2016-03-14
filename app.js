482fb7a63e24921c04a0776ca33d378fc9ddee9e
ywbshiwo <785534746@qq.com>
2016-03-14 16:32:26 +0800

    add something cool

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var config = require("./config/Server");
 var port = process.env.PORT||config.port;
 var routes = require("./routes");
 var Member = require("./model/member");
+var compression = require('compression');
 var db;
+
 
 mongoose.connect('mongodb://localhost:27017/test');
 db = mongoose.connection;
@@ -16,6 +18,7 @@ db.once('open', function() {
     console.log("we connect!")
 });
 //������ͼĿ¼
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views","./views");
